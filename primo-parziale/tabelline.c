037f6d5b966ee34d98f1aab0a19a199142ec9799
Francesco Antoni <f.antoni@consorziometis.it>
2025-10-27 00:38:04 +0100

    Aggiunte prove primo parziale

---
+++ primo-parziale/tabelline.c
@@ -0,0 +1,60 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*Esercizio 1 Scrivere un programma tabelline che invocato scrivendo
+
+tabelline n1 n2 n2 ....
+per ogni intero N sulla linea di comando crea il file N.tab contenente la tabellina (i multipli da 1 a 10) del numero N. Ad esempio scrivendo
+
+tabelline 4 20
+deve essere creato il file 4.tab contenente la sequenza
+
+4
+8
+12
+16
+20
+24
+28
+32
+36
+40
+ed il file 20.tab contenente la sequenza
+
+20
+40
+60
+80
+100
+120
+140
+160
+180
+200
+I file di output devono chiamarsi esattamente come indicato, e contenere esattamente i valori richiesti con un valore su ogni riga.
+Il programma deve accettare un qualsiasi numero di argomenti sulla linea di comando. Verificate con valgrind il corretto uso e deallocazione della memoria
+ */
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s n...\n", argv[0]);
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        int num = atoi(argv[i]);
+        const size_t len = strlen(argv[i]) + 5;
+        char *filename = malloc(sizeof(char) * len);
+        assert(filename != NULL);
+        sprintf(filename, "%i.tab", num);
+        FILE *output = fopen(filename, "w");
+        assert(output != NULL);
+        for (int j = 1; j <= 10; j++) {
+            fprintf(output, "%i\n", j*num);
+        }
+        fclose(output);
+        free(filename);
+    }
+}
---
+++ primo-parziale/triangolo.c
@@ -0,0 +1,70 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+Scrivere un programma triangolo che invocato scrivendo:
+
+triangolo nomefile
+legge da nomefile le stringhe contenute in esso e le salva in un array. Successivamente crea un file nomefile.triangolo
+contenente nella riga i-esima le parole dalla i-esima alla prima in ordine inverso. Ad esempio, se il file di input si
+chiama moria e contiene le parole dite amici ed entrate il file di output deve chiamarsi moria.triangolo e contentere le 4 righe:
+
+dite
+amici dite
+ed amici dite
+entrate ed amici dite
+L'esercizio verrà considerato corretto solamente se la memoria verrà gestita in maniera dinamica e correttamente
+deallocata (verrà utilizzato valgrind per la verifica).
+*/
+
+int main(const int argc, const char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <nomefile>\n", argv[0]);
+        return 1;
+    }
+
+    char **parole = NULL;
+
+    FILE *f = fopen(argv[1], "r");
+    assert(f != NULL);
+
+    int n;
+    
+    for (n = 0; !feof(f); n++) {
+        char **tmp = realloc(parole, sizeof(char *) * (n + 1));
+        if (tmp == NULL) {
+            perror("La realloc è fallita!\n");
+            free(parole);
+            fclose(f);
+            return 1;
+        }
+        parole = tmp;
+        fscanf(f, "%ms", &parole[n]);
+    }
+
+    fclose(f);
+
+    assert(parole != NULL);
+
+    const size_t s = strlen(argv[1]) + strlen(".triangolo") + 1;
+    char *filename = malloc(sizeof(char) * s);
+    assert(filename != NULL);
+    sprintf(filename, "%s.triangolo", argv[1]);
+    f = fopen(filename, "w");
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j >= 0; j--) {
+            fprintf(f, "%s ", parole[j]);
+        }
+        fprintf(f, "\n");
+    }
+
+    for (int i = 0; i < n; ++i) {
+        free(parole[i]);
+    }
+    free(parole);
+    free(filename);
+    fclose(f);
+}
